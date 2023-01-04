91f752761cc04b624fb53d62f85672e7729346af
Jayson Rhynas <jayrhynas@gmail.com>
2023-01-04 14:42:18 -0500

    Day 5 - Part 2

--- Sources/day5/part2.swift
+++ Sources/day5/part2.swift
@@ -6,6 +6,25 @@ struct Part2: ParsableCommand {
     var input: Input
 
     mutating func run() throws {
+        var (stacks, moves) = try input.read()
         
+        apply(moves, to: &stacks)
+
+        let topCrates = stacks.map {
+            $0.last ?? ""
+        }
+
+        print(topCrates.joined(separator: ""))
+    }
+
+    func apply(_ moves: [Move], to stacks: inout [Stack]) {
+        for move in moves {
+            let fromIdx = move.from - 1
+            
+            let crates = stacks[fromIdx].suffix(move.count)
+            stacks[fromIdx].removeLast(move.count)
+
+            stacks[move.to - 1].append(contentsOf: crates)
+        }
     }
 }
